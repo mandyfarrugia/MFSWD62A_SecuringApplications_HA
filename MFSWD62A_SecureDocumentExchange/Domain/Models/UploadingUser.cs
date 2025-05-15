5014c8c831a16c44d2c671eca2a2d1561470726e
mandyfarrugia <mandy.farrugia.c10352@mcast.edu.mt@DESKTOP-0TN6NU4>
2025-05-15 16:29:03 +0200

    Starting with a model class representing a user who will upload a document. This will inherit from IdentityUser containing the password field.

--- MFSWD62A_SecureDocumentExchange/Domain/Models/UploadingUser.cs
+++ MFSWD62A_SecureDocumentExchange/Domain/Models/UploadingUser.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace Domain.Models
 {
-    public class UploadingUser
+    public class UploadingUser : IdentityUser
     {
+        [Required, MinLength(2), MaxLength(25)]
+        public string FirstName { get; set; }
+
+        [Required, MinLength(2), MaxLength(50)]
+        public string LastName { get; set; }
     }
 }
