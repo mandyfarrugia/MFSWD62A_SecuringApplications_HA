91d8f240db5f9fa8279f98e1205c4efebb5b0e42
mandyfarrugia <mandy.farrugia.c10352@mcast.edu.mt@DESKTOP-0TN6NU4>
2025-05-15 17:40:26 +0200

    Defined the gateway to the database.

--- MFSWD62A_SecureDocumentExchange/DataAccess/Context/DocumentExchangeDbContext.cs
+++ MFSWD62A_SecureDocumentExchange/DataAccess/Context/DocumentExchangeDbContext.cs
@@ -4,13 +4,29 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Context
 {
-    public class DocumentExchangeDbContext(DbContextOptions options) : IdentityDbContext(options)
+    /// <summary>
+    /// This class is the gateway to the database. Therefore, all connections and management of the database are done here.
+    /// The instanation of an object of this class establishes a connection to the database.
+    /// </summary>
+    /// <param name="options">An instance of DbContextOptions stores information pertaining to configuration.
+    /// For this assignment, it will set up the database provider targeting Microsoft SQL Server and the connection string pointing to the database.
+    /// This instance can be used to configure Entity Framework through an ASP.NET Core application, unit testing with in-memory databases, as well applying Dependency Injection via said instance to inject the instance of DbContext, in this case DocumentExchangeDbContext.</param>
+    public class DocumentExchangeDbContext(DbContextOptions<DocumentExchangeDbContext> options) : IdentityDbContext<UploadingUser>(options)
     {
+        /// <summary>
+        /// This property is used to access the UploadingUser table in the database to perform CRUD operations.
+        /// </summary>
         public DbSet<UploadingUser> UploadingUsers { get; set; }
 
+        /// <summary>
+        /// OnModelCreating is used to handle the mapping model classes in C# .NET to database tables, in this case in SQL Server.
+        /// It will also configure the relationships between the tables (such as one-to-many, as well as tables (name of tables, column names and types, as well constraints).
+        /// Case in point, this method can be used to set unique instead, as well generating a NEWID() for a UNIQUEIDENTIFIER column (tantamount to C# .NET's Guid type).
+        /// </summary>
+        /// <param name="builder">This method uses the parameter of type ModelBuilder to configure entity types (tables), properties (columns), relationships, keys and indexes, constraints, as well as seeding of initial data.</param>
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(builder);
+            base.OnModelCreating(builder); //Helps ensure that tables related to authentication (Identity) are created properly.
         }
     }
 }
--- MFSWD62A_SecureDocumentExchange/Domain/Models/UploadingUser.cs
+++ MFSWD62A_SecureDocumentExchange/Domain/Models/UploadingUser.cs
@@ -1,18 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace Domain.Models
 {
+    /// <summary>
+    /// This model class embodies a user who wishes to upload a document via the web application.
+    /// </summary>
     public class UploadingUser : IdentityUser
     {
+        /// <summary>
+        /// This property is used to store the first name of the user.
+        /// A not null constraint has been assigned through the Required data annotation attribute.
+        /// Values must contain at least 2 characters but no more than 25 characters.
+        /// </summary>
         [Required, MinLength(2), MaxLength(25)]
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// This property is used to store the last name of the user.
+        /// A not null constraint has been assigned through the Required data annotation attribute.
+        /// Values must contain at least 2 characters but no more than 50 characters.
+        /// </summary>
         [Required, MinLength(2), MaxLength(50)]
         public string LastName { get; set; }
     }
